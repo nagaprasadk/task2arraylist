8e29f6d4f59d70d2224ee2df87e89e4315d6561c
nagaprasadk <85783081+nagaprasadk@users.noreply.github.com>
2022-03-17 18:30:46 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            int a = sc.nextInt();
+            arr.add(a);
+        }
+    }
+}
